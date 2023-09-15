893c8263175cef0b573997f443555565b6d30751
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-15 23:13:53 +0700

    feat: process object

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const message = (name) => {
-  console.log(`Hello ${name}!`);
-};
+const initialMemoryUsage = process.memoryUsage().heapUsed;
+const yourName = process.argv[2];
+const environment = process.env;
 
-message('JavaScript');
+for (let i = 0; i <= 10000; i++) {
+  // Proses looping ini akan membuat penggunaan memori naik
+}
+
+const currentMemoryUsage = process.memoryUsage().heapUsed;
+
+console.log(`Halo ${yourName}`);
+console.log(`Mode environment: ${environment}`);
+console.log(
+  `Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`
+);
