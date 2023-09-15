235ce6150ebdb10f6dce746df4de8f402ede2a12
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-15 23:23:25 +0700

    chore: move process object into folder

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const initialMemoryUsage = process.memoryUsage().heapUsed;
-const yourName = process.argv[2];
-const environment = process.env;
+const message = (name) => {
+  return `Hello ${name}!`;
+};
 
-for (let i = 0; i <= 10000; i++) {
-  // Proses looping ini akan membuat penggunaan memori naik
-}
-
-const currentMemoryUsage = process.memoryUsage().heapUsed;
-
-console.log(`Halo ${yourName}`);
-console.log(`Mode environment: ${environment}`);
-console.log(
-  `Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`
-);
+message('World');
